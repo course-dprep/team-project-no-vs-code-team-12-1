db6fc6744f155b046119137643c122b8560339ec
Lý Bá Thọ <135591799+LyBaTho@users.noreply.github.com>
2024-09-20 15:21:16 +0200

    prepare_data.R

--- prepare_data.R
+++ prepare_data.R
@@ -5,27 +5,27 @@ library(googledrive)
 
 ### Input ### 
 
-# File URL with direct download
-business_dataset <- 'https://drive.google.com/uc?export=download&id=12o5mGJV8ck_Kqi_x23WF1HbsNGIP70ru'
-user_dataset <- 'https://drive.google.com/uc?export=download&id=1g-Sy1IMEqrPtPtcc1t2U78iR9Eh8EMtC'
-review_dataset <- 'https://drive.google.com/uc?export=download&id=1nlI5cioa3Q6uZhzEdD5ZvXq_FlvsM1wL'
+# File ID from Google Drive URLs
+business_file_id <- '12o5mGJV8ck_Kqi_x23WF1HbsNGIP70ru'
+user_file_id <- '1g-Sy1IMEqrPtPtcc1t2U78iR9Eh8EMtC'
+review_file_id <- '1nlI5cioa3Q6uZhzEdD5ZvXq_FlvsM1wL'
 
-# Temporary file to save the download
-temp_business <- tempfile(fileext = ".csv")
-temp_user <- tempfile(fileext = ".csv")
-temp_review <- tempfile(fileext = ".csv")
+# Paths to save the files in the working directory
+business_file <- "business_data.csv"
+user_file <- "user_data.csv"
+review_file <- "review_data.csv"
 
-# Download the files
-download.file(business_dataset, destfile = temp_business, mode = "wb")
-download.file(user_dataset, destfile = temp_user, mode = "wb")
-download.file(review_dataset, destfile = temp_review, mode = "wb")
+# Download files using googledrive package
+drive_download(as_id(business_file_id), path = business_file, overwrite = TRUE)
+drive_download(as_id(user_file_id), path = user_file, overwrite = TRUE)
+drive_download(as_id(review_file_id), path = review_file, overwrite = TRUE)
 
 # Load the CSV files into variables
-business_data <- read_csv(temp_business)
-user_data <- read_csv(temp_user)
-review_data <- read_csv(temp_review)
+business_data <- read_csv(business_file)
+user_data <- read_csv(user_file)
+review_data <- read_csv(review_file)
 
-# View the first few rows
+# Optional: View the first few rows
 head(business_data)
 head(user_data)
 head(review_data)
