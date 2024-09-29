9e2c87037a97163381852837a77693727383bb38
LyBaTho <lyba.tho2412@gmail.com>
2024-09-29 13:01:30 +0200

    add download packages

---
+++ 6-source_code/0_install_packages.R
@@ -0,0 +1,12 @@
+# WORKING DIRECTORY SETTING PACKAGES
+install.packages("here")
+install.packages("rstudioapi")
+
+# GENERAL PACKAGES
+install.packages("tidyverse")
+install.packages("googledrive")
+install.packages("readr")
+install.packages("dplyr")
+
+# R MARKDOWN RENDER
+install.packages("reticulate")
